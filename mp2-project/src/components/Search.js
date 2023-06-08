2d6cb6bf00704b9edf6ad1bb18daf76f42004229
Johnskyle Sebastian <bestiejohn10@gmail.com>
2023-06-08 20:38:50 +0800

    Savepoint

--- mp2-project/src/components/Search.js
+++ mp2-project/src/components/Search.js
@@ -44,6 +44,7 @@ function SearchMovie() {
     overview.innerText = selectedData.movieOverview
     container.current.style.background = `linear-gradient(90deg, #000d 20%, #0003), url(${selectedData.backgroundImg})`
     poster.current.src = selectedData.posterImg
+    console.log('click')
 
   }
 
